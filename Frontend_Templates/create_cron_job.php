9f5327cbce888f889e9c82eddb9ac3489ddbb2a5
Obrin24 <gjohnsonjan2002@gmail.com>
2024-04-01 16:47:36 -0500

    Update create_cron_job.php

--- Frontend_Templates/create_cron_job.php
+++ Frontend_Templates/create_cron_job.php
@@ -14,11 +14,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $cron_job_run_script = "$schedule $run_script_command";
 
         // Add the cron jobs to the crontab
-        file_put_contents('/tmp/crontab.txt', $cron_job_create_db . PHP_EOL, FILE_APPEND);
-        file_put_contents('/tmp/crontab.txt', $cron_job_run_script . PHP_EOL, FILE_APPEND);
+        file_put_contents('/var/www/crontab.txt', $cron_job_create_db . PHP_EOL, FILE_APPEND);
+        file_put_contents('/var/www/crontab.txt', $cron_job_run_script . PHP_EOL, FILE_APPEND);
 
         // Install updated crontab
-        exec('crontab /tmp/crontab.txt');
+        exec('crontab /var/www/crontab.txt');
 
         echo "Cron jobs scheduled successfully for schedule: $schedule";
     } else {
